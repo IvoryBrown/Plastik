13be55ced5ee04a0ff9034e25c5380709b77aae5
SzervizIvo <paradisho@gmail.com>
2019-03-26 21:58:21 +0100

    init

--- Plastik/src/com/office/commodityrollweight/controller/RollWeightController.java
+++ Plastik/src/com/office/commodityrollweight/controller/RollWeightController.java
@@ -19,18 +19,20 @@ public class RollWeightController implements Initializable {
 	@FXML
 	private Label rollCalculationWeightLbl, rollCalculationSpoolsWeightLbl, rollDiameterLbl, message;
 	@FXML
+	private Label rollSpoolDiameterLbl, rollDiameterMesLbl;
+	@FXML
 	private ComboBox<Integer> rollNumberOfLayersTxt;
 	private NumberCheck checkNumber = new NumberCheck();
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		rollNumberOfLayersTxt.getItems().addAll(setCombobox());
+		rollNumberOfLayersTxt.getSelectionModel().selectFirst();
 		checkNumber.setQuantityNumber(rollSpoolDiameterTxt, message);
 		checkNumber.setQuantityNumber(rollWidth, message);
 		checkNumber.setQuantityNumber(rollThicknessTxt, message);
 		checkNumber.setQuantityNumber(rollLengthTxt, message);
-		checkNumber.isDouble(rollSpoolsWeightTxt, message);
-
+		setOnTextField();
 	}
 
 	private Integer[] setCombobox() {
@@ -38,15 +40,48 @@ public class RollWeightController implements Initializable {
 		return CMBDEVICENAME;
 	}
 
+	private void setOnTextField() {
+		rollSpoolDiameterTxt.textProperty().addListener((observable, oldValue, newValue) -> {
+			rollSpoolDiameterLbl.setText(newValue);
+			if (newValue.length() >= 3) {
+				String s = newValue.substring(0, 3);
+				rollSpoolDiameterLbl.setText(s);
+				rollSpoolDiameterTxt.setText(s);
+			}
+		});
+	}
+
+	private Integer textFieldNumeric(String text) {
+		Integer i = Integer.valueOf(text);
+		return i;
+
+	}
+
+	private Double textFieldObule(String text) {
+		Double i = Double.valueOf(text);
+		return i;
+
+	}
+
 	@FXML
 	private void upload() {
-		Integer cseveatmero = 100;
-		Integer reteg = 2;
-		Integer szelesseg = 2300;
-		Integer vastagsag = 80;
-		Integer hossz = 1400;
-		double csevesuly = 1.65;
-		double foljasuly =(hossz*(szelesseg/1000)*reteg*vastagsag*(10^(-6))*920);
-		System.out.println(foljasuly);
+		NumberFormat formatter = new DecimalFormat("#0.00");
+		NumberFormat formatter1 = new DecimalFormat("#0");
+		double z = Math.pow(10, -6);
+		double y = Math.pow(textFieldObule(rollSpoolDiameterTxt.getText()) / 1000, 2);
+		rollCalculationWeightLbl.setText(String.valueOf(formatter.format(textFieldNumeric(rollLengthTxt.getText())
+				* ((textFieldObule(rollWidth.getText()) / 1000) * rollNumberOfLayersTxt.getValue()
+						* textFieldNumeric(rollThicknessTxt.getText()) * (z) * 920))));
+		rollCalculationSpoolsWeightLbl.setText(String.valueOf(formatter.format(textFieldNumeric(rollLengthTxt.getText())
+				* ((textFieldObule(rollWidth.getText()) / 1000) * rollNumberOfLayersTxt.getValue()
+						* textFieldNumeric(rollThicknessTxt.getText()) * (z) * 920)
+				+ textFieldObule(rollSpoolsWeightTxt.getText()))));
+		rollDiameterLbl
+				.setText(
+						String.valueOf(formatter1.format(Math
+								.sqrt((((textFieldNumeric(rollLengthTxt.getText()) * rollNumberOfLayersTxt.getValue())
+										* (textFieldNumeric(rollThicknessTxt.getText()) * z) / 3.14) * 4 + (y)))
+								* 1000)));
+
 	}
 }
